53d7fa3a6c310b5174ef3b8ff4b30ae77a6cee5e
romanibra <romanman242@gmail.com>
2024-02-18 20:01:35 +0600

    keyboard half done

--- scripts/alpha.clash.js
+++ scripts/alpha.clash.js
@@ -1,3 +1,28 @@
+// function play(){
+//    const homeSection=document.getElementById('home');
+//    homeSection.classList.add('hidden')
+
+// //    play ground
+// const playgroundSection=document.getElementById('play-ground')
+// playgroundSection.classList.remove('hidden')
+// // console.log(playgroundSection)
+// }
+ 
+function continueGame(){
+    // step-1 generate a random alphabet
+    const alphabet =getARandomAlphabet();
+    console.log('your random alphabet',alphabet)
+
+    // randomly generate alphabet to the screen
+    const currentAlphabetElement=document.getElementById('current-alphabet')
+    currentAlphabetElement.innerText=alphabet;
+    // set background color
+setBackgroundColorId(alphabet);
+
+}
+
 function play(){
-    console.log('play start now')
+    hideElementById('home')
+    showElementById('play-ground')
+    continueGame()
 }
