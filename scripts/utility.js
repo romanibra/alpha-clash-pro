6477b61630aa418dbf5300645ac0a22e27456671
romanibra <romanman242@gmail.com>
2024-02-19 12:30:40 +0600

    project done

--- scripts/utility.js
+++ scripts/utility.js
@@ -11,6 +11,29 @@ function setBackgroundColorId(elementId){
 const element =document.getElementById(elementId);
 element.classList.add('bg-orange-400');
 }
+function removeBackgroundColorId(elementId){
+const element =document.getElementById(elementId);
+element.classList.remove('bg-orange-400');
+}
+
+function getTextElementValueById(elementId){
+    const element =document.getElementById(elementId);
+    const elementValueText=element.innerText;
+    const value=parseInt(elementValueText);
+    return value;
+}
+function setTextElementValueById(elementId, value){
+    const element =document.getElementById(elementId);
+    element.innerText= value;
+}
+
+function getElementTextById(elementId){
+    const element=document.getElementById(elementId);
+    const text=element.innerText;
+    return text;
+}
+
+
 
 function getARandomAlphabet(){
     // get or create an alphabet array
